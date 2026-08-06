4bdefc98f95304ba0931fda0f871fe708d2db183
Rushi <rushisurose83@gmail.com>
2026-08-06 07:00:03 +0530

    ll from scratch

--- Linked List/lec1.cpp
+++ Linked List/lec1.cpp
@@ -1,7 +1,60 @@
+// Linked List From Scratch
+
 #include<iostream>
 using namespace std;
-                 
-int main() {
+
+class Node {
+public :
+    int data ;
+    Node* next ;  // Node* Memory Adress of Object of Node Class 
+
+    Node(int val){
+        data = val ;
+        next = NULL ;
+    }
+
+};
+
+class List {
+    Node* head ;
+    Node* tail ;
+
+public :
+    List(){
+        head = tail = NULL ;
+    }
+
+    void push_front(int val){
+        Node* newNode = new Node(val); // Dynamic 
+        // Node newNode(val) ; // Static 
+        if (head == NULL)
+        {
+            head = tail = newNode ;
+            return ;
+        }
+        else {
+            newNode-> next = head ;  //(*newNode).next = head
+            head = newNode ;
+        }
         
+    }
+
+    void print(){
+        Node* temp = head ;
+        while (temp != NULL)
+        {
+            cout << temp-> data << " " ;
+            temp = temp-> next ;
+        }
+        
+    }
+};
+
+int main() {
+    List ll ;
+    ll.push_front(1);
+    ll.push_front(2);
+    ll.push_front(3);  
+    ll.print() ;  
     return 0;
 }
