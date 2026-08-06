b719e865b01de145f44c766a346b6246c4cd4b98
Rushi <rushisurose83@gmail.com>
2026-08-06 19:07:50 +0530

    search and insert function from scratch

--- Linked List/lec1.cpp
+++ Linked List/lec1.cpp
@@ -87,6 +87,53 @@ public :
         
     }
 
+    void insert(int val ,int idx){
+        if(idx < 0) return ;
+        if(idx==0) {
+            push_front(val) ;
+            return ;
+        }
+
+        // new node formation
+        Node* newNode = new Node(val);
+
+        // store address of LL
+        Node* temp = head ;
+
+        // update temp till idx-1 so that it will point just previous val of insertion position
+        for (int i = 0; i < idx-1; i++)
+        {
+            if (temp == NULL)
+            {
+                cout << "INVALID Position" << endl;
+                return ;
+            }
+            
+            temp = temp-> next ;
+        }
+
+        // establish connection betn newNode and insertion Position (temp-> next point to insertion position)
+        newNode-> next = temp-> next ;
+
+        // establish connection betn prev position of insertion position and newNode
+        temp-> next = newNode ;
+        
+    }
+
+    int search(int val){
+        Node* temp = head ;
+        int idx = 0 ;
+        while (temp != NULL)
+        {
+            if(temp->data==val) {
+                return idx;
+            }
+            temp = temp-> next ;
+            idx ++ ;
+        }
+        return -1 ;
+    }
+
     void print(){
         Node* temp = head ;
         while (temp != NULL)
@@ -94,6 +141,7 @@ public :
             cout << temp-> data << " " ;
             temp = temp-> next ;
         }
+        cout << "->NULL" << endl ;
         
     }
 };
@@ -103,7 +151,8 @@ int main() {
     ll.push_back(1);
     ll.push_back(2);
     ll.push_back(3);
-    ll.pop_back();  
+    ll.insert(4,1); 
+    cout << ll.search(2) << endl; 
     ll.print() ;
     return 0;
 }
