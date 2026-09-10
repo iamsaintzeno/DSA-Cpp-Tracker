fcd05983015af1e492cbc0093fe0a2259585a08e
Rushi <rushisurose83@gmail.com>
2026-09-10 19:52:22 +0530

    previous_smaller.cpp added

---
+++ Stack/previous_smaller.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+
+vector<int> prevSmaller(vector<int>& arr){
+    int n = arr.size();
+    vector<int> ans(n);
+    stack<int> s ;
+    for (int i = 0; i < n; i++)
+    {
+        while (s.size()>0 && s.top()>arr[i])
+        {
+            s.pop();
+        }
+        if (s.size()==0)
+        {
+            ans[i] = -1 ;
+        }
+        else{
+            ans[i] = s.top();
+        }
+        s.push(arr[i]);
+        
+    }
+    return ans ;
+    
+}
+                 
+int main() {
+    vector<int> arr = {3,1,0,8,6};
+    vector<int> ans = prevSmaller(arr);
+    for(int val : ans){
+        cout << val << " ";
+    }       
+    cout << endl ; 
+    return 0;
+}
